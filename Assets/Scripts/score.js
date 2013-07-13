c847db2092bcdc1339960823dfa686f774463c0b
OminStyle <name.cmh@gmail.com>
2013-07-13 14:56:01 -0700

    Fixed GUI script

--- Assets/Scripts/score.js
+++ Assets/Scripts/score.js
@@ -1,5 +1,7 @@
 #pragma strict
 
+var textureToDisplay : Texture2D;
+
 function Start () {
 
 }
@@ -9,7 +11,7 @@ function Update () {
 }
 
 function OnGUI () {
-        GUI.Label (Rect (10, 10, 100, 20), "Score: " + Player.distanceTraveled);
+        GUI.Label (Rect (300, 10, 100, 20), "Score: " + Player.distanceTraveled);
 
     GUI.Label (Rect (10, 10, textureToDisplay.width/3, textureToDisplay.height/3),
         textureToDisplay);
