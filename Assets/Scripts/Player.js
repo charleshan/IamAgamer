cbc1b60cbbf805dc3c39bed33f24a98af1362cf2
OminStyle <name.cmh@gmail.com>
2013-07-13 11:58:09 -0700

    test

--- Assets/Scripts/Player.js
+++ Assets/Scripts/Player.js
@@ -1,15 +1,36 @@
 #pragma strict
 
-function Start () {
+var velocity: float;
+var movementSpeed: float = 5;
+var isGrounded: boolean = false;
+var jumpVelocity: Vector3;
 
+
+function Start () {
+    
 }
 
 function Update () {
-    if(Input.GetButtonDown("Vertical"))
-        print("");
+    transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed, 0, 0);
+
+    if (Input.GetButtonDown("Jump"))
+    {
+        rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+		isGrounded = false;
+    }
 }
 
 function FixedUpdate()
 {
-    rigidbody.AddForce(0, 0, 10);
+    rigidbody.AddForce(0, 0, velocity);
+}
+
+function OnCollisionEnter() 
+{
+    isGrounded = true;
+}
+
+function OnCollisionExit()
+{
+    isGrounded = false;
 }
