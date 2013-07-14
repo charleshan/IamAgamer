f047fd109106207e2c7f959247b94960427430e0
Akash <akash.peri@gmail.com>
2013-07-13 17:25:42 -0700

    Commit changes for Obstacles
    Updated the way obstacles are generated

--- Assets/Scripts/GenerateObstacles.js
+++ Assets/Scripts/GenerateObstacles.js
@@ -1,11 +1,11 @@
 #pragma strict
 
 public var obstacle:Transform;
-var timer:float = 1;
 
 var usedObjectQueue = new Queue();
 var newObjectQueue = new Queue();
-var recycleOffset = 30;
+var recycleOffset = 80;
+var stopGeneration:boolean = false;
 
 function Start () {
 	
@@ -13,39 +13,83 @@ function Start () {
 
 function Update () 
 {
-
 	var player:GameObject;
 	player = GameObject.Find("Player");
 	var travelled = player.GetComponent(Player).distanceTraveled;
 	
-	timer -= Time.deltaTime;
-	if(timer <= 0)
+	if(Mathf.Floor(travelled%20) == 0 && !stopGeneration)
 	{
-		var newObs:Transform;
-		if(newObjectQueue.Count == 0)
-		{
-			newObs = Instantiate (obstacle, Vector3(Random.Range(-9,9), 1, travelled+30), Quaternion.identity);
-		}
-		else
-		{
-			newObs = newObjectQueue.Dequeue();
-			newObs.transform.position.x = Random.Range(-9,9);
-			newObs.transform.position.z = travelled+30;
-		}
-		timer = 1;
-		usedObjectQueue.Enqueue(newObs);
+		CreateScenario(1,travelled+30);
+		
+		stopGeneration = true;
+	}
+	else if(stopGeneration && Mathf.Floor(travelled%20) != 0)
+	{
+		stopGeneration = false;
 	}
 	
 	if(usedObjectQueue.Count > 0)
 	{
 		var firstObs:Transform = usedObjectQueue.Peek();
 	
-		if(firstObs.position.x + recycleOffset < travelled)
+		if(firstObs.position.z + recycleOffset < travelled)
 		{
 			usedObjectQueue.Dequeue();
 			newObjectQueue.Enqueue(firstObs);
 		}
 	}
+}
+
+function CreateScenario (obstacleType : int, distanceToPlace : int)
+{
+	print("Yay");
+	var obstacle:Transform;
+	obstacle = GenerateObstacle();
+	switch(obstacleType)
+	{
+		case 0:
+			//repurpose obstacle to jumpable block (assume 1b1b1)
+			obstacle.transform.position.x = Random.Range(-9,9);
+			obstacle.transform.position.y = 5;
+			obstacle.transform.position.z = distanceToPlace+30;
+			
+			obstacle.transform.localScale.x = 2;
+			obstacle.transform.localScale.y = 4;
+			obstacle.transform.localScale.z = 2;
+			break;
+		case 1:
+			//repurpose obstacle to unpassable obstacle
+			obstacle.transform.position.x = Random.Range(-9,9);
+			obstacle.transform.position.y = 3;
+			obstacle.transform.position.z = distanceToPlace+30;
+			
+			obstacle.transform.localScale.x = 2;
+			obstacle.transform.localScale.y = 6;
+			obstacle.transform.localScale.z = 2;
+			break;
+		default:
+			print("something broke");
+			break;
+	}
+}
+
+function GenerateObstacle ()
+{
+	var player:GameObject;
+	player = GameObject.Find("Player");
+	var travelled = player.GetComponent(Player).distanceTraveled;
+
+	var newObs:Transform;
+	if(newObjectQueue.Count == 0)
+	{
+		newObs = Instantiate (obstacle, Vector3(Random.Range(-9,9), 1, travelled+60), Quaternion.identity);
+	}
+	else
+	{
+		newObs = newObjectQueue.Dequeue();
+	}
+	usedObjectQueue.Enqueue(newObs);
+	return newObs;
 }
 
 /*using UnityEngine;
